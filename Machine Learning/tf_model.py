4a7093561512601a1f54f17515903f217271e1a9
Muhammad Fadel Akbar Putra <muhammad.fadel02@ui.ac.id>
2022-12-12 10:36:13 +0700

    upload

--- Machine Learning/tf_model.py
+++ Machine Learning/tf_model.py
@@ -4,12 +4,9 @@ from tensorflow.keras.preprocessing import image
 from PIL import Image, ImageFile
 from io import BytesIO
 
-quality_model=load_model(r'C:\Users\Melchior Natthan\Documents\Mirror\SIVITAS AKADEMIKA\SEMESTER 5\RPL\Fruitify\Machine Learning\model1\local_rotten_lr2_final.h5')
-clf_model=load_model(r'C:\Users\Melchior Natthan\Documents\Mirror\SIVITAS AKADEMIKA\SEMESTER 5\RPL\Fruitify\Machine Learning\model1\local_rotten_lr2_final.h5')
+quality_model=load_model(r'C:\Users\fadel\Documents\data rpl\model1\local_rotten_lr2_final.h5')
+clf_model=load_model(r'C:\Users\fadel\Documents\data rpl\model1\local_fruit_final.h5')
 
-
-# reads frfom file object
-# return array of original uploaded image and 1x100x100x3 processed image
 def preprocess(file):
     ImageFile.LOAD_TRUNCATED_IMAGES =False
     org_img=Image.open(BytesIO(file.read()))
@@ -21,24 +18,23 @@ def preprocess(file):
     return org_img, np.expand_dims(img,axis=0)
 
 
-# return [prob_for_fresh, prob_for_rotten]
 def check_rotten(img):
-    #return [round(100*quality_model.predict(img)[0][0],3),round(100*(1-quality_model.predict(img)[0][0]),3)]
     rotten_dict={}
     rotten_dict['fresh']= round(100*quality_model.predict(img)[0][0],3)
     rotten_dict['rotten']= round(100*(1-quality_model.predict(img)[0][0]),3)
 
     return rotten_dict
 
-# return dict... {'apple':prob, 'banana':prob, 'orange':prob}
 def classify_fruit(img):
     fru_dict={}
     fru_dict['apple']=round(clf_model.predict(img)[0][0]*100,4)
     fru_dict['banana']=round(clf_model.predict(img)[0][1]*100,4)
     fru_dict['orange']=round(clf_model.predict(img)[0][2]*100,4)
 
+    max_val = max(fru_dict, key=fru_dict.get)
+
     for value in fru_dict:
      if fru_dict[value]<=0.001:
         fru_dict[value]=0.00
 
-    return fru_dict
+    return max_val
